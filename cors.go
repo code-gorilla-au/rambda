ba457b1abddde9ce1033c0fb12c4cb787e4ff1a5
mauricio riquelme <m.riquelme223@gmail.com>
2022-07-09 11:21:26 +1000

    rename for clarity

--- cors.go
+++ cors.go
@@ -1,9 +1,21 @@
 package rambda
 
-import "github.com/aws/jsii-runtime-go"
+import (
+	"github.com/aws/jsii-runtime-go"
+)
 
-var defaultCors = &map[string]*string{
-	"method.response.header.Access-Control-Allow-Headers": jsii.String("'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"),
-	"method.response.header.Access-Control-Allow-Origin":  jsii.String("'*'"),
-	"method.response.header.Access-Control-Allow-Methods": jsii.String("'OPTIONS,GET,PUT,POST,DELETE'"),
+const (
+	defaultCorsAllowedOrigins string = "*"
+	defaultCorsAllowedMethods string = "OPTIONS,GET,PUT,POST,PATCH,DELETE"
+	defaultCorsAllowedHeaders string = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent"
+)
+
+// DefaultHeaders - provide sane defaults for rest api headers
+func DefaultHeaders() *string {
+	return jsii.String(defaultCorsAllowedHeaders)
+}
+
+// DefaultMethods - provide sane defaults for rest api methods
+func DefaultMethods() *string {
+	return jsii.String(defaultCorsAllowedMethods)
 }
